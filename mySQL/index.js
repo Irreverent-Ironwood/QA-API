ba07b647b33b9be562161ae7fa0088a62da82c55
Michael Moriarty <mmoriarty316@gmail.com>
2021-03-24 14:17:54 -0700

    added data to gitignore

--- mySQL/index.js
+++ mySQL/index.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 // Initialize pool
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host: '127.0.0.1',
+  host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'qna',
